6f61c1a7c71444ed2063948d4a399061d715c1e5
nivivirat <imniveditha1188@gmail.com>
2024-01-08 20:25:40 +0530

    side bar close option

--- src/Components/Common/SideBar/SideBar.jsx
+++ src/Components/Common/SideBar/SideBar.jsx
@@ -18,7 +18,7 @@ export default function SideBar() {
 
     return (
         <div className="fixed top-0 left-0 z-20">
-            <div className={`${isOpen ? 'w-[300px] h-screen' : 'w-screen h-[80px]'} transition-all duration-500`}>
+            <div className={`${isOpen ? 'w-screen h-screen' : 'w-screen h-[80px]'} transition-all duration-500`}>
 
                 {!isOpen ? (
                     <div className="flex flex-row justify-between border-b bg-base shadow-lg">
@@ -36,27 +36,28 @@ export default function SideBar() {
                 ) : (
 
                     // side bar
-                    <div className="bg-base h-full z-50 p-3">
-                        <div className="flex flex-row justify-between">
-                            <img className='sm:h-[45px] sm:w-[90px] h-6 w-10' src='https://res.cloudinary.com/dzhdarh4q/image/upload/v1699014385/Reifenhauser/ReifenhauserLogo_hhc7wi.svg' alt='Logo' />
-                            <button className="text-primary text-3xl p-4" onClick={toggleSidebar} >
-                                <Icon icon="tabler:x" />
-                            </button>
-                        </div>
+                    <div className="z-40" onClick={()=>{setIsOpen(false)}}>
+                        <div className="bg-base h-full z-50 p-3 w-[300px]">
+                            <div className="flex flex-row justify-between">
+                                <img className='sm:h-[45px] sm:w-[90px] h-6 w-10' src='https://res.cloudinary.com/dzhdarh4q/image/upload/v1699014385/Reifenhauser/ReifenhauserLogo_hhc7wi.svg' alt='Logo' />
+                                <button className="text-primary text-3xl p-4" onClick={toggleSidebar} >
+                                    <Icon icon="tabler:x" />
+                                </button>
+                            </div>
 
-                        <div>
-                            <ul className='flex flex-col gap-5 mt-5'>
-                                <Nav title={"Home"} destination={""} />
-                                <Nav title={"About Us"} destination={"AboutUs"} />
-                                <Nav title={"Services"} destination={"Services"} />
-                                <Nav title={"Principals"} destination={"Principals"} />
-                                <Nav title={"Events"} destination={"Events"} />
-                                <Nav title={"Media"} destination={"Media"} />
-                                <Nav title={"Careers"} destination={"Careers"} />
-                                <Nav title={"Contact us"} destination={"ContactUs"} />
-                            </ul>
+                            <div>
+                                <ul className='flex flex-col gap-5 mt-5'>
+                                    <Nav title={"Home"} destination={""} />
+                                    <Nav title={"About Us"} destination={"AboutUs"} />
+                                    <Nav title={"Services"} destination={"Services"} />
+                                    <Nav title={"Principals"} destination={"Principals"} />
+                                    <Nav title={"Events"} destination={"Events"} />
+                                    <Nav title={"Media"} destination={"Media"} />
+                                    <Nav title={"Careers"} destination={"Careers"} />
+                                    <Nav title={"Contact us"} destination={"ContactUs"} />
+                                </ul>
+                            </div>
                         </div>
-
                     </div>
                 )}
             </div>
